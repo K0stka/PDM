b54ba9747940c6ef5375aa024e33c66544585496
K0stka <www.honza.90@gmail.com>
2025-01-20 10:02:25 +0100

    fix2

--- app/private/shared/workshops/page.tsx
+++ app/private/shared/workshops/page.tsx
@@ -1,4 +1,12 @@
-import { archetypes as Archetypes, interests as Interests, count, db, eq, events, getTableColumns } from "@/db";
+import {
+    archetypes as Archetypes,
+    interests as Interests,
+    countDistinct,
+    db,
+    eq,
+    events,
+    getTableColumns,
+} from "@/db";
 import { canUserExpressInterest, interestsActive } from "@/validation/interest";
 
 import { Card } from "@/components/ui/card";
@@ -9,44 +17,53 @@ import { configuration } from "@/configuration/configuration";
 import { session } from "@/auth/session";
 
 const WorkshopsPage: NextPage = async () => {
-	const user = await session();
+    const user = await session();
 
-	const archetypes = await db
-		.select({
-			...getTableColumns(Archetypes),
-			interested: count(Interests.id),
-			events: count(events.id),
-		})
-		.from(Archetypes)
-		.leftJoin(Interests, eq(Archetypes.id, Interests.archetype))
-		.leftJoin(events, eq(Archetypes.id, events.archetype))
-		.groupBy(Archetypes.id)
-		.orderBy(Archetypes.name);
+    const archetypes = await db
+        .select({
+            ...getTableColumns(Archetypes),
+            interested: countDistinct(Interests.id),
+            events: countDistinct(events.id),
+        })
+        .from(Archetypes)
+        .leftJoin(Interests, eq(Archetypes.id, Interests.archetype))
+        .leftJoin(events, eq(Archetypes.id, events.archetype))
+        .groupBy(Archetypes.id)
+        .orderBy(Archetypes.name);
 
-	const interests = user.isAttending
-		? await db.query.interests.findMany({
-				where: eq(Interests.user, user.id),
-				columns: {
-					archetype: true,
-				},
-		  })
-		: [];
+    const interests = user.isAttending
+        ? await db.query.interests.findMany({
+              where: eq(Interests.user, user.id),
+              columns: {
+                  archetype: true,
+              },
+          })
+        : [];
 
-	return (
-		<PageTemplate title="Anotace přednášek">
-			<div className="flex flex-col gap-5">
-				{interestsActive() && user.isAttending && <Card className="p-5 text-muted-foreground">{configuration.interestsCTA}</Card>}
-				{archetypes.map((archetype) => (
-					<SharedArchetypeElement
-						key={archetype.id}
-						archetype={archetype}
-						canExpressInterest={canUserExpressInterest(user, interests.length)}
-						isInterested={interests.some((interest) => interest.archetype === archetype.id)}
-					/>
-				))}
-			</div>
-		</PageTemplate>
-	);
+    return (
+        <PageTemplate title="Anotace přednášek">
+            <div className="flex flex-col gap-5">
+                {interestsActive() && user.isAttending && (
+                    <Card className="p-5 text-muted-foreground">
+                        {configuration.interestsCTA}
+                    </Card>
+                )}
+                {archetypes.map((archetype) => (
+                    <SharedArchetypeElement
+                        key={archetype.id}
+                        archetype={archetype}
+                        canExpressInterest={canUserExpressInterest(
+                            user,
+                            interests.length,
+                        )}
+                        isInterested={interests.some(
+                            (interest) => interest.archetype === archetype.id,
+                        )}
+                    />
+                ))}
+            </div>
+        </PageTemplate>
+    );
 };
 
 export default WorkshopsPage;
